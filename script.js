5142cf5816ecb0ddf46826a0c194e7c28af652bf
github_awkwardlynumb <pearce.matthewa@gmail.com>
2020-07-01 20:24:28 -0700

    pseudo code started

---
+++ script.js
@@ -0,0 +1,9 @@
+//fill text in jumbotron with the date the user is viewing it
+//assign styles via css class for past, present and future time slots
+//
+
+moment().format('MMMM Do YYYY, h:mm:ss a'); // July 1st 2020, 7:32:24 pm
+moment().format('dddd');                    // Wednesday
+moment().format("MMM Do YY");               // Jul 1st 20
+moment().format('YYYY [escaped] YYYY');     // 2020 escaped 2020
+moment().format();                         
